6caa1a13a1bd2d5fc6ee4534f3300c06505055f9
PawelIm7403 <pawel.im7403@gmail.com>
2022-04-25 00:04:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/androidgametutorial/Time.java
@@ -0,0 +1,13 @@
+package com.example.androidgametutorial;
+
+public final class Time {
+
+    public static long resolution() {
+        return 1000000000;
+    }
+
+    public static long now() {
+        return System.nanoTime();
+    }
+
+}
